853d95dac1cb08f858579a5dafa6f6cfa11caad9
Swagat Kumar Mishra ✦ <kumarmishraswagat415@gmail.com>
2025-08-08 00:16:52 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyB9QUKJVEkxig7oi8u9A2z1bbwrgSJHw_A",
-  authDomain: "documagic-4e803.firebaseapp.com",
-  projectId: "documagic-4e803",
-  storageBucket: "documagic-4e803.firebasestorage.app",
-  messagingSenderId: "697027141753",
-  appId: "1:697027141753:web:3b3ca85ff656f37bcbb4a8"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 });
 
 const messaging = firebase.messaging();
