67221f5964f1f4f58b61dfdfea2a8818f92f1d92
Swagat Kumar Mishra ✦ <kumarmishraswagat415@gmail.com>
2025-08-08 20:56:44 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: 'icon-192.png',
+    badge: 'icon-192.png'
+  };
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
