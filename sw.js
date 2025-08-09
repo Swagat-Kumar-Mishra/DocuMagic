111f9771959cd44859d54136e74d6984c2ddf837
Swagat Kumar Mishra ✦ <kumarmishraswagat415@gmail.com>
2025-08-09 11:43:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('push', event => {
     body: data.body,
     icon: 'icon-192.png', // Small icon in the notification header
     badge: 'notification-badge.png', // Monochrome icon for the status bar
-    image: data.image // The large image displayed in the notification body
+    image: data.image // The large image displayed in the notification body (e.g., the Google Drive URL)
   };
   event.waitUntil(
     self.registration.showNotification(data.title, options)
