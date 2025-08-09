21d4cfd7521fd3cf95b399fdf44b9a27b931ebce
Swagat Kumar Mishra ✦ <kumarmishraswagat415@gmail.com>
2025-08-09 11:37:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ self.addEventListener('push', event => {
   const data = event.data.json();
   const options = {
     body: data.body,
-    icon: 'icon-192.png',
-    badge: 'icon-192.png'
+    icon: 'icon-192.png', // Small icon in the notification header
+    badge: 'notification-badge.png', // Monochrome icon for the status bar
+    image: data.image // The large image displayed in the notification body
   };
   event.waitUntil(
     self.registration.showNotification(data.title, options)
